ae2b937986e3b5cb8ba5164906fab5d93740efe7
VenuTallapally <thalla.venu06@gmail.com>
2016-08-05 11:57:53 +0530

    Code Challenge

---
+++ CodeChallenge/CodeChallenge/CodeChallenge01Aug16.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CodeChallenge
+{
+    public class CodeChallenge01Aug16
+    {
+        public int FindIndex(int[] numbers)
+        {
+            var index = -1;
+            var leftSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var rightSum = 0;
+                if(i != 0) leftSum += numbers[i-1];
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+                if (leftSum == rightSum)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ CodeChallenge/CodeChallengeTests/CodeChallenge01Aug16Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CodeChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeChallengeTests
+{
+    [TestClass]
+    public class CodeChallenge01Aug16Tests
+    {
+        [TestMethod]
+        public void MustReturnIndex()
+        {
+            var codeChallenge = new CodeChallenge01Aug16();
+            var numbers = new []{ 4,8,9,7,5,5,23 };
+
+            var index = codeChallenge.FindIndex(numbers);
+            Console.WriteLine(index);
+            Console.WriteLine(index == -1 ? "TEST CASE: FAILED" : "TEST CASE: PASSED");
+        }
+    }
+}
